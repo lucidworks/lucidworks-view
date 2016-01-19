4c59a7b9b6d05ce0ede60b3775b020e0f9b40022
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-19 15:58:55 -0800

    Refactor for ease of reading

--- client/assets/js/services/ConfigService.js
+++ client/assets/js/services/ConfigService.js
@@ -33,78 +33,6 @@ angular.module('fusionSeedApp.services.config', [])
     /* initialize on first load */
     init();
 
-    /**
-     * Extend config with the defaults
-     */
-    function init(){
-      appConfig = _.assign({}, CONFIG_DEFAULT, CONFIG_OVERRIDE);
-    }
-
-    var getFusionUrl = function(){
-      return appConfig.host + ':' + appConfig.port;
-    };
-
-    var getQueryPipeline = function(){
-      return appConfig.queryPipelineIdList[0];
-    };
-
-    var getQueryProfile = function(){
-      return appConfig.queryProfilesIdList[0];
-    };
-
-    var getLoginCredentials = function(){
-      return {
-        username: appConfig.user,
-        passowrd: appConfig.password
-      };
-    };
-
-    var getCollectionName = function(){
-      return appConfig.collection;
-    };
-
-    var getLabels = function(){ //TODO: Decide whether defined labels will be the only ones shown
-      return appConfig.labels;
-    };
-
-    /**
-     * Returns all the config properties that
-     * ends with a `_field` which is not a blank string
-     * and is toggled by explicit enable-ment by `_enabled` of the same type
-     */
-    var getAllFields = function(){
-      var fieldsMap = {};
-      _.keys(appConfig).filter(function(item){
-        return item.match(/\_field$/);
-      }).filter(function(item){
-        var key = item.split('_')[0]+'_enabled';
-        return _.has(appConfig, key)?appConfig[key]:true;
-      }).filter(function(item){
-        return _.trim(appConfig[item])!=='';
-      }).forEach(function(keyName){
-        fieldsMap[keyName] = appConfig[keyName];
-      });
-      return fieldsMap;
-    };
-
-
-    /**
-     * [function Returns specific field of given type]
-     * @param  {[string]} fieldType [the type of field that needs to be fetch from the config]
-     * @return {[type]}           [the value of the said field or null if not found]
-     */
-    var getSpecificField = function(fieldType){
-      var allFields = getAllFields();
-      if(!fieldType.match(/\_field$/)){
-        fieldType += '_field';
-      }
-      return allFields[fieldType]?allFields[fieldType]:null;
-    };
-
-    var getAuthHeader = function(){
-      return appConfig.authorizationHeader;
-    };
-
     return {
       init: init, //TODO: Only for test env
       config: appConfig,
@@ -119,4 +47,76 @@ angular.module('fusionSeedApp.services.config', [])
         get: getSpecificField
       }
     };
+    
+    /**
+     * Extend config with the defaults
+     */
+    function init(){
+      appConfig = _.assign({}, CONFIG_DEFAULT, CONFIG_OVERRIDE);
+    }
+
+    function getFusionUrl(){
+      return appConfig.host + ':' + appConfig.port;
+    }
+
+    function getQueryPipeline(){
+      return appConfig.queryPipelineIdList[0];
+    }
+
+    function getQueryProfile(){
+      return appConfig.queryProfilesIdList[0];
+    }
+
+    function getLoginCredentials(){
+      return {
+        username: appConfig.user,
+        passowrd: appConfig.password
+      };
+    }
+
+    function getCollectionName(){
+      return appConfig.collection;
+    }
+
+    function getLabels(){ //TODO: Decide whether defined labels will be the only ones shown
+      return appConfig.labels;
+    }
+
+    /**
+     * Returns all the config properties that
+     * ends with a `_field` which is not a blank string
+     * and is toggled by explicit enable-ment by `_enabled` of the same type
+     */
+    function getAllFields(){
+      var fieldsMap = {};
+      _.keys(appConfig).filter(function(item){
+        return item.match(/\_field$/);
+      }).filter(function(item){
+        var key = item.split('_')[0]+'_enabled';
+        return _.has(appConfig, key)?appConfig[key]:true;
+      }).filter(function(item){
+        return _.trim(appConfig[item])!=='';
+      }).forEach(function(keyName){
+        fieldsMap[keyName] = appConfig[keyName];
+      });
+      return fieldsMap;
+    }
+
+
+    /**
+     * [function Returns specific field of given type]
+     * @param  {[string]} fieldType [the type of field that needs to be fetch from the config]
+     * @return {[type]}           [the value of the said field or null if not found]
+     */
+    function getSpecificField(fieldType){
+      var allFields = getAllFields();
+      if(!fieldType.match(/\_field$/)){
+        fieldType += '_field';
+      }
+      return allFields[fieldType]?allFields[fieldType]:null;
+    }
+
+    function getAuthHeader(){
+      return appConfig.authorizationHeader;
+    }
   });
