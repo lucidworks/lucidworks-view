af6085d0a3560da17ad4d594ca857e45dce024bc
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-19 15:59:54 -0800

    Rewrite SolrService to proper format

--- client/assets/js/services/SolrService.js
+++ client/assets/js/services/SolrService.js
@@ -3,11 +3,12 @@ angular.module('fusionSeedApp.services.solr', ['fusionSeedApp.services.config'])
   .service('SolrService', function(ConfigService){
     console.log(ConfigService.getFusionURL());
 
-    var makeQuery = function(query, queryParams){
-      return $http.get(ConfigService.getFusionURL()); //TODO
-    };
-
     return {
       makeQuery: makeQuery
     };
+
+    function makeQuery(query, queryParams){
+      return $http.get(ConfigService.getFusionURL()); //TODO
+    }
+
   });
