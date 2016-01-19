a5fdea6069293c5663637f0e7e55db7e176e6ec4
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-18 20:03:49 -0800

    formatting

--- client/assets/js/services/ConfigApiService.js
+++ client/assets/js/services/ConfigApiService.js
@@ -3,10 +3,13 @@ angular.module('application')
     var configData = appConfig;
     console.log(configData);
 
-    var getFusionRoot = function(){
-      //TODO: Do something
-    };
     return {
       getFusionRoot: getFusionRoot
     };
+
+    function getFusionRoot(){
+      //TODO: Do something
+    }
+
+
   });
