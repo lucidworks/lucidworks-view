52b8272ac58ebd6578fdd8b6ae4971587f863af1
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-19 17:44:16 -0800

    Fix config tests

--- tests/assets/js/services/ConfigApiServiceTest.js
+++ tests/assets/js/services/ConfigApiServiceTest.js
@@ -40,48 +40,48 @@ ngDescribe({
 
     describe('getAllFields() with existing config properties',function(){
       it('should return according to the _enabled=true', function(){
-        deps.ConfigApiService.init({
+        deps.ConfigService.init({
           thumbnails_enabled: true,
           thumbnails_field: 'thumb_url'
         });
-        expect(deps.ConfigApiService.getFields.all().thumbnails_field).toEqual('thumb_url');
+        expect(deps.ConfigService.getFields.all().thumbnails_field).toEqual('thumb_url');
       });
 
       it('should return nothing according to the _enabled=false', function(){
-        deps.ConfigApiService.init({
+        deps.ConfigService.init({
           thumbnails_enabled: false,
           thumbnails_field: 'thumb_url'
         });
-        expect(deps.ConfigApiService.getFields.all().thumbnails_field).toEqual(undefined);
+        expect(deps.ConfigService.getFields.all().thumbnails_field).toEqual(undefined);
       });
 
       it('should return nothing if it\'s blank and enabled', function(){
-        deps.ConfigApiService.init({
+        deps.ConfigService.init({
           thumbnails_enabled: true,
           thumbnails_field: ''
         });
-        expect(deps.ConfigApiService.getFields.all().thumbnails_field).toEqual(undefined);
+        expect(deps.ConfigService.getFields.all().thumbnails_field).toEqual(undefined);
       });
 
       it('should return properly crafted objects', function(){
-        deps.ConfigApiService.init({
+        deps.ConfigService.init({
           thumbnails_enabled: true,
           thumbnails_field: 'thumb',
           image_field: 'image',
           image_enabled: true
         });
-        expect(deps.ConfigApiService.getFields.all()).toEqual({
+        expect(deps.ConfigService.getFields.all()).toEqual({
           thumbnails_field: 'thumb',
           image_field: 'image'
         });
 
-        deps.ConfigApiService.init({
+        deps.ConfigService.init({
           thumbnails_enabled: true,
           thumbnails_field: 'thumb',
           image_field: 'image',
           image_enabled: false
         });
-        expect(deps.ConfigApiService.getFields.all()).toEqual({
+        expect(deps.ConfigService.getFields.all()).toEqual({
           thumbnails_field: 'thumb'
         });
       });
