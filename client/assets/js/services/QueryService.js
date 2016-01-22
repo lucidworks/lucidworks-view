179f9406e0f84655601060873e9e60d35c7ae221
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-21 18:35:16 -0800

    Refactor query service

--- client/assets/js/services/QueryService.js
+++ client/assets/js/services/QueryService.js
@@ -2,6 +2,10 @@
   angular.module('fusionSeedApp.services.query',
     ['fusionSeedApp.services.config', 'fusionSeedApp.services.apiBase'])
     .config(Config)
+    .constant('QUERY_OBJECT_DEFAULT', {
+      // Force return of JSON
+      wt: 'json'
+    })
     .provider('QueryService', QueryService);
 
     Config.$inject = ['OrwellProvider'];
@@ -12,16 +16,55 @@
 
     function QueryService(){
 
-      this.$get = ['$q', '$http', 'ConfigService', 'ApiBase', $get];
+      this.$get = ['$q', '$http', 'ConfigService', 'ApiBase', 'Orwell', 'QUERY_OBJECT_DEFAULT', $get];
 
       /////////////
 
-      function $get($q, $http, ConfigService, ApiBase){
+      function $get($q, $http, ConfigService, ApiBase, Orwell, QUERY_OBJECT_DEFAULT){
+        var queryObservable = Orwell.getObservable('query');
         return {
           getQuery: getQuery
         };
 
-        // Internal functions
+        /**
+         * Make a query to the query profiles endpoint
+         * @param  {object} query  Should have all the query params, like
+         *                         For select?q=query&fq=blah you need to pass in an object
+         *                         {'q': 'query', 'fq': 'blah'}
+         * @return {Promise}       Promise that resolve with a Fusion response coming from Solr
+         */
+        function getQuery(query){
+          var deffered = $q.defer();
+
+          var queryObject = _.assign({}, QUERY_OBJECT_DEFAULT, query);
+
+          var queryString = QueryObjectToString(queryObject);
+
+          var fullUrl = getQueryUrl(ConfigService.getIfQueryProfile()) + '?' + queryString;
+
+          $http.get(fullUrl)
+            .then(success)
+            .catch(failure);
+
+          function success(response){
+            // Set the content to populate the rest of the ui.
+            queryObservable.setContent(response.data);
+            deffered.resolve(response.data);
+          }
+
+          function failure(err){
+            deffered.reject(err.data);
+          }
+
+          return deffered.promise;
+        }
+
+        /**
+         * Returns the appropriate base url for an endpoint
+         *
+         * @param  {Boolean} isProfiles Determines which endpoint type to return;
+         * @return {string}             The URL endpoint for the query without parameters.
+         */
         function getQueryUrl(isProfiles){
           var profilesEndpoint = ApiBase.getEndpoint() +
             '/api/apollo/collections/' +
@@ -41,33 +84,16 @@
         }
 
         /**
-         * Make a query to the query profiles endpoint
-         * @param  {object} query  Should have all the query params, like
-         * For select?q=query&fq=blah you need to pass in an object
-         * {'q': 'query', 'fq': 'blah'}
-         * @return {Promise}       Promise that resolve with a Fusion response coming from Solr
+         * Turns a Query Object into a String
+         * @param {object} obj The query object to turn into a string
+         * @return {string}
          */
-        function getQuery(query){
-          var deffered = $q.defer();
-
-          var queryObject = angular.copy(query);
-          queryObject.wt='json'; //Force JSON returns
-          var queryString = _.reduce(queryObject, function(str, value, key){
+        function QueryObjectToString(obj){
+          return _.reduce(obj, function(str, value, key){
             return str + ((str!=='')?'&':'') + key + '=' + value;
           },'');
-
-          var fullUrl = getQueryUrl(ConfigService.getIfQueryProfile()) + '?' + queryString;
-
-          $http.get(fullUrl)
-            .then(function(response){
-              deffered.resolve(response.data);
-            })
-            .catch(function(err){
-              deffered.reject(err.data);
-            });
-
-          return deffered.promise;
         }
+
       }
     }
 })();
