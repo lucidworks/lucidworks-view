5f192abedc48c8a125b39908a083239c758f2a79
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-22 18:14:45 -0800

    eslint pass on auth interceptor

--- client/assets/js/services/AuthInterceptor.js
+++ client/assets/js/services/AuthInterceptor.js
@@ -1,47 +1,31 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('fusionSeedApp.services.authInterceptor', ['fusionSeedApp.services.config'])
-        .service('AuthInterceptor', AuthInterceptor)
-        .service('SessionInjector', SessionInjector);
+  angular
+    .module('fusionSeedApp.services.authInterceptor', ['fusionSeedApp.services.config'])
+    .factory('AuthInterceptor', AuthInterceptor);
 
-    AuthInterceptor.$inject = ['$q', '$log', '$injector'];
-    SessionInjector.$inject = ['ConfigService'];
+  AuthInterceptor.$inject = ['$q', '$log', '$injector'];
 
-    /* @ngInject */
-    function SessionInjector(ConfigService) {
-      var sessionInjector = {
-        request: function(config) {
-          _.assign(config.headers, ConfigService.getAuthHeader());
-          return config;
-        }
-      };
-      return sessionInjector;
-    }
+  /* @ngInject */
+  function AuthInterceptor($q, $log, $injector) {
 
-    /* @ngInject */
-    function AuthInterceptor($q, $log, $injector) {
+    return {
+      responseError:  responseError,
+    };
 
-      return {
-        responseError:  responseError,
-      };
+    //////////////
 
-      //////////////
-
-
-      function responseError(response){
-        var deferred = $q.defer();
-        var $state = $injector.get('$state');
-        if(!$state.is('login') && (resp.status === 401 || resp.status === 403)){
-            deferred.reject(resp);
-            $state.go('login');
-          }
-
-
-        return deferred.promise;
+    function responseError(resp){
+      var deferred = $q.defer();
+      var $state = $injector.get('$state');
+      if(!$state.is('login') && (resp.status === 401 || resp.status === 403)) {
+        deferred.reject(resp);
+        $state.go('login');
       }
 
-
+      return deferred.promise;
     }
+
+  }
 })();
