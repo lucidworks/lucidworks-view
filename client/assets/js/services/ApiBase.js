c2cdf28f59d133c087348c5691ccef42a42e2e83
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-22 18:10:28 -0800

    lint apibase

--- client/assets/js/services/ApiBase.js
+++ client/assets/js/services/ApiBase.js
@@ -1,33 +1,33 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('fusionSeedApp.services.apiBase', [])
-        .provider('ApiBase', ApiBase);
+  angular
+    .module('fusionSeedApp.services.apiBase', [])
+    .provider('ApiBase', ApiBase);
 
-    ApiBase.$inject = [];
+  ApiBase.$inject = [];
 
-    /* @ngInject */
-    function ApiBase() {
-      var self = this;
-      self.endpoint =  '';
-      self.$get = get;
-      self.setEndpoint = setEndpoint;
-      self.getEndpoint = getEndpoint;
+  /* @ngInject */
+  function ApiBase() {
+    var self = this;
+    self.endpoint =  '';
+    self.$get = get;
+    self.setEndpoint = setEndpoint;
+    self.getEndpoint = getEndpoint;
 
-      function setEndpoint(endpoint) {
-        self.endpoint = endpoint;
-      }
-
-      function getEndpoint(){
-        return self.endpoint;
-      }
-
-      function get() {
-        return {
-          getEndpoint: getEndpoint,
-          setEndpoint: setEndpoint
-        };
-      }
+    function setEndpoint(endpoint) {
+      self.endpoint = endpoint;
     }
+
+    function getEndpoint(){
+      return self.endpoint;
+    }
+
+    function get() {
+      return {
+        getEndpoint: getEndpoint,
+        setEndpoint: setEndpoint
+      };
+    }
+  }
 })();
