37bb1cbefd01049fda50e4cc95a156ad88e45b8b
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-22 16:15:12 -0800

    add version script

---
+++ gulp/version.js
@@ -0,0 +1,12 @@
+var $               = require('gulp-load-plugins')();
+var gulp            = require('gulp');
+var argv            = require('yargs').argv;
+
+gulp.task('version', function(){
+  return gulp.src("package.json")
+    .pipe($.jsonEditor(function(json) {
+      json.version = argv.version;
+      return json; // must return JSON object.
+    }))
+    .pipe(gulp.dest("."));
+});
