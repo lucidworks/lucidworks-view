1736085495ee1767e6254750327b2e489728703c
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-25 12:25:37 -0800

    Update query service

--- client/assets/js/services/QueryService.js
+++ client/assets/js/services/QueryService.js
@@ -1,98 +1,67 @@
 /*global _*/
-(function(){
-  angular.module('fusionSeedApp.services.query',
-    ['fusionSeedApp.services.config', 'fusionSeedApp.services.apiBase'])
+(function () {
+  angular.module('fusionSeedApp.services.query', ['fusionSeedApp.services.config',
+      'fusionSeedApp.services.queryData'
+    ])
     .config(Config)
     .constant('QUERY_OBJECT_DEFAULT', {
-      // Force return of JSON
+      q: '*:*',
+      start: 0,
+      rows: 20,
+      // Do not override the return of JSON
       wt: 'json'
     })
     .provider('QueryService', QueryService);
 
   Config.$inject = ['OrwellProvider'];
 
-  function Config(OrwellProvider){
-    OrwellProvider.createObservable('query',{});
+  function Config(OrwellProvider) {
+    OrwellProvider.createObservable('query', {});
   }
 
-  function QueryService(){
+  function QueryService() {
 
-    this.$get = ['$q', '$http', 'ConfigService', 'ApiBase', 'Orwell', 'QUERY_OBJECT_DEFAULT', $get];
+    this.$get = ['ConfigService', 'Orwell', 'QUERY_OBJECT_DEFAULT', 'QueryDataService',
+      '$log', $get
+    ];
 
     /////////////
 
-    function $get($q, $http, ConfigService, ApiBase, Orwell, QUERY_OBJECT_DEFAULT){
-      var queryObservable = Orwell.getObservable('query');
+    function $get(ConfigService, Orwell, QUERY_OBJECT_DEFAULT, QueryDataService, $log) {
+      var queryObservable = Orwell.getObservable('query'),
+        queryObject = QUERY_OBJECT_DEFAULT;
+
+      $log.log(queryObject);
+
+      init();
+
+      queryObservable.addObserver(function (query) {
+        $log.debug(query);
+        QueryDataService.getQueryResults(query);
+      });
+
       return {
-        getQuery: getQuery
+        setQuery: setQuery,
+        getQueryObservable: getQueryObservable,
+        getQueryObject: getQueryObject
       };
 
-      /**
-       * Make a query to the query profiles endpoint
-       * @param  {object} query  Should have all the query params, like
-       *                         For select?q=query&fq=blah you need to pass in an object
-       *                         {'q': 'query', 'fq': 'blah'}
-       * @return {Promise}       Promise that resolve with a Fusion response coming from Solr
-       */
-      function getQuery(query){
-        var deferred = $q.defer();
-
-        var queryObject = _.assign({}, QUERY_OBJECT_DEFAULT, query);
-
-        var queryString = QueryObjectToString(queryObject);
-
-        var fullUrl = getQueryUrl(ConfigService.getIfQueryProfile()) + '?' + queryString;
-
-        $http.get(fullUrl)
-          .then(success)
-          .catch(failure);
-
-        function success(response){
-          // Set the content to populate the rest of the ui.
-          queryObservable.setContent(response.data);
-          deferred.resolve(response.data);
-        }
-
-        function failure(err){
-          deferred.reject(err.data);
-        }
-
-        return deferred.promise;
+      function init() {
+        queryObservable.setContent(queryObject);
       }
 
-      /**
-       * Returns the appropriate base url for an endpoint
-       *
-       * @param  {Boolean} isProfiles Determines which endpoint type to return;
-       * @return {string}             The URL endpoint for the query without parameters.
-       */
-      function getQueryUrl(isProfiles){
-        var profilesEndpoint = ApiBase.getEndpoint() +
-          'api/apollo/collections/' +
-          ConfigService.getCollectionName() +
-          '/query-profiles/' +
-          ConfigService.getQueryProfile() +
-          '/select';
-
-        var pipelinesEndpoint = ApiBase.getEndpoint() +
-          'api/apollo/query-pipelines/' +
-          ConfigService.getQueryPipeline() +
-          '/collection/' +
-          ConfigService.getCollectionName() +
-          '/select';
-
-        return isProfiles?profilesEndpoint:pipelinesEndpoint;
+      function getQueryObservable() {
+        return queryObservable;
       }
 
-      /**
-       * Turns a Query Object into a String
-       * @param {object} obj The query object to turn into a string
-       * @return {string}
-       */
-      function QueryObjectToString(obj){
-        return _.reduce(obj, function(str, value, key){
-          return str + ((str!=='')?'&':'') + key + '=' + value;
-        },'');
+      function getQueryObject() {
+        return queryObservable.getContent();
+      }
+
+      function setQuery(query) {
+        queryObject = _.assign({}, queryObject, query);
+        queryObservable.setContent(queryObject);
+
       }
 
     }
