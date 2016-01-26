9af6a2beaba813613dd22443bf28f85f11654945
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-26 09:55:56 -0800

    Add signal service

---
+++ client/assets/js/services/SignalService.js
@@ -0,0 +1,50 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('fusionSeedApp.service.signals', ['fusionSeedApp.services.apiBase',
+      'fusionSeedApp.services.config'
+    ])
+    .factory('SignalsService', SignalsService);
+
+  SignalsService.$inject = ['ApiBase', 'ConfigService', '$http', '$q', 'QueryService'];
+
+  /* @ngInject */
+  function SignalsService(ApiBase, ConfigService, $http, $q, QueryService) {
+    var service = {
+      postSignal: postSignal
+    };
+
+    return service;
+
+    function postSignal(documentId) {
+      var deferred = $q.defer();
+      var date = new Date();
+      var data = [{
+        params: {
+          query: QueryService.getQueryObject()
+            .q,
+          docId: documentId, //
+        },
+        type: ConfigService.config.signalType,
+        timestamp: date.toISOString()
+      }];
+
+      $http
+        .post(ApiBase.getEndpoint() + 'api/apollo/signals/' + ConfigService.config.signalsPipeline,
+          data)
+        .then(success)
+        .catch(failure);
+
+      function success(response) {
+        deferred.resolve(response.data);
+      }
+
+      function failure(err) {
+        deferred.reject(err.data);
+      }
+
+      return deferred.promise;
+    }
+  }
+})();
