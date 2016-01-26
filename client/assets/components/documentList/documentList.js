833d9c221d43ba7f1161a91d9247ff8b88ec308f
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-01-25 19:25:09 -0800

    pagination fixes

--- client/assets/components/documentList/documentList.js
+++ client/assets/components/documentList/documentList.js
@@ -12,18 +12,24 @@
     return {
       restrict: 'EA',
       templateUrl: 'assets/components/documentList/documentList.html',
+      link: linkFunc,
       controller: Controller,
       controllerAs: 'vm',
       bindToController: true,
-      scope: true
+      scope: true,
+      replace: true
     };
+
+    function linkFunc(scope, el, attr, ctrl){
+
+    }
 
   }
 
-  Controller.$inject = ['$log', '$scope', 'ConfigService', 'QueryService', 'Orwell'];
+  Controller.$inject = ['$log', '$scope', '$anchorScroll', 'ConfigService', 'QueryService', 'Orwell'];
 
   /* @ngInject */
-  function Controller($log, $scope, ConfigService, QueryService, Orwell) {
+  function Controller($log, $scope, $anchorScroll, ConfigService, QueryService, Orwell) {
     var vm = this;
     vm.docs = [];
 
@@ -38,6 +44,7 @@
         } else {
           vm.docs = [];
         }
+        $anchorScroll('topOfDocList');
       });
     }
   }
