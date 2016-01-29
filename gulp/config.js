8ecb0109573ec8bb5ab83befea02fdfe65ba7d8d
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2016-01-29 23:45:38 +0530

    Added gulp task to get authorization header for FUSION_CONFIG.js
    To use,
    `gulp setupAuthHeader --username=[admin] --password=[password123] --realm=[native] --hostname=[localhost:8764]`
    
    It will give you a replacement `authorizationHeader` property that you can paste in your config in your terminal.

--- gulp/config.js
+++ gulp/config.js
@@ -3,13 +3,16 @@ var log             = require('connect-logger');
 var gulp            = require('gulp');
 var fs              = require('fs');
 
-// Copies your app's page templates and generates URLs for them
+// Copies your app's vonfig
 gulp.task('copy:config', function() {
   if(fs.existsSync(global.paths.configJS[0])){ //If the file exists use that, or copy from the sample
     return gulp.src(global.paths.configJS).pipe(gulp.dest('./build/assets/js/'));
   }
   else {
-    return gulp.src(global.paths.configJSSample).pipe($.rename('FUSION_CONFIG.js')).pipe(gulp.dest('./')).pipe(gulp.dest('./build/assets/js/'));
+    return gulp.src(global.paths.configJSSample)
+      .pipe($.rename('FUSION_CONFIG.js'))
+      .pipe(gulp.dest('./'))
+      .pipe(gulp.dest('./build/assets/js/'));
   }
 });
 
