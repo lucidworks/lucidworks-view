b3ad5eae60612106de3e9e091455718331b876e6
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-02-29 14:52:43 -0800

    update services to reduce dupes and add missing

--- client/assets/js/services/services.js
+++ client/assets/js/services/services.js
@@ -5,9 +5,8 @@ angular.module('fusionSeedApp.services', [
   'fusionSeedApp.services.authInterceptor',
   'fusionSeedApp.services.config',
   'fusionSeedApp.services.landingPage',
+  'fusionSeedApp.services.link',
   'fusionSeedApp.services.query',
   'fusionSeedApp.services.queryData',
-  'fusionSeedApp.services.auth',
-  'fusionSeedApp.services.link',
-  'fusionSeedApp.services.authInterceptor'
+  'fusionSeedApp.service.signals'
 ]);
