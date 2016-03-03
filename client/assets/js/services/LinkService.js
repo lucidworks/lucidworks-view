09e94630eb2550f0bdd91bbe0c1d18188e212947
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2016-03-03 21:08:29 +0530

    Minor tweaks

--- client/assets/js/services/LinkService.js
+++ client/assets/js/services/LinkService.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('fusionSeedApp.services.link', [])
+    .module('fusionSeedApp.services.link', ['rison'])
     .constant('BLANK_QUERY', blankQuery)
     .constant('QUERY_PARAM', 'query')
     .factory('LinkService', LinkService);
@@ -21,13 +21,13 @@
     };
 
     function setQuery(queryObject) {
+      QueryService.setQuery(queryObject);
       var queryObjectString = $rison.stringify(QueryService.getQueryObject());
       var newStateObject = {};
       newStateObject[QUERY_PARAM] = queryObjectString;
       // Adding reloadOnSearch:false for now fixes the double reload bug SU-60
       // @see http://stackoverflow.com/a/22863315
       $state.go('home', newStateObject, {notify: false, reloadOnSearch: false});
-      QueryService.setQuery(queryObject);
     }
 
     function getQueryFromUrl() {
