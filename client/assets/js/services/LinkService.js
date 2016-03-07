4b961ea89dee05ab844c69596edd70bbd7d2f9b6
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2016-03-07 23:16:21 +0530

    SU-72 Fixed the problem with array/object ambiguity
    Since in Rison there are no distinct difference between strings and numbers, and array and object
     - Wrote routines that will check for possible arrays in the Rison object and convert them to object.
     - That object will be identical with the object that generate that query

--- client/assets/js/services/LinkService.js
+++ client/assets/js/services/LinkService.js
@@ -36,9 +36,53 @@
       return newQueryObject;
     }
 
+    function isArrayable(item){
+      var stuff = _.keys(item);
+
+      var stuffToCheck = _.chain(stuff).map(function(value){
+        return parseInt(value);
+      })
+      .filter(function(value){
+        return _.isNumber(value);
+      })
+      .value();
+      return stuff.length === stuffToCheck.length;
+    }
+
+    function convertToArray(item){
+      var newArray = [];
+      var newArrayLength = 0;
+      _.forIn(item, function(item, key){
+        newArrayLength++;
+        newArray[key] = item;
+      });
+      newArray.length = newArrayLength;
+      return newArray;
+    }
+
+    function treatArrays(queryObject){
+      var newQueryObject = _.isArray(queryObject)?[]:{};
+      _.forIn(queryObject, function(item, key){
+        if(_.isObject(item) || _.isArray(item)){
+          //Checking if the object could be an array by checking all the keys are integers
+          //Rison specs are inadequate to decide, hence this piece of function
+          var tempObject = treatArrays(item);
+          if(_.isObject(item) && isArrayable(item)){
+            tempObject = convertToArray(item);
+          }
+          newQueryObject[key] = tempObject;
+        }
+        else{
+          newQueryObject[key] = item;
+        }
+      });
+      return newQueryObject;
+    }
+
     function setQuery(queryObject) {
       QueryService.setQuery(queryObject);
       var queryObjectToBeStringed = _.clone(QueryService.getQueryObject(),true);
+      $log.info('queryObjectToBeStringed',queryObjectToBeStringed);
       //Only need the slashes to get encoded, so that app state doesn't change
       queryObjectToBeStringed = encodeSlashes(queryObjectToBeStringed);
       var queryObjectString = $rison.stringify(queryObjectToBeStringed);
@@ -51,7 +95,8 @@
 
     function getQueryFromUrl() {
       var queryString = $location.search()[QUERY_PARAM];
-      return queryString ? $rison.parse(decodeURIComponent(queryString)) : BLANK_QUERY;
+      var queryObject = queryString ? $rison.parse(decodeURIComponent(queryString)):BLANK_QUERY;
+      return treatArrays(queryObject);
     }
   }
 })();
