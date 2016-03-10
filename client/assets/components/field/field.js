8c391ba42a5160252e09e3792923f2f6f8afe86c
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-03-09 16:52:01 -0800

    Formatting (no logic change)

--- client/assets/components/field/field.js
+++ client/assets/components/field/field.js
@@ -16,7 +16,7 @@
       bindToController: {
         value: '=',
         highlight: '=',
-        hkey: '@',  //The Highlight key, used to look up the proper highlighting snippet by name.
+        hkey: '@', //The Highlight key, used to look up the proper highlighting snippet by name.
         maxlength: '='
       },
       replace: true
@@ -32,11 +32,6 @@
 
     ///////////
 
-    function handleField(e, docId, score, position){
-      //dc.postSignal(signalId);
-
-    }
-
     function activate() {
       //console.log("Field:");
       //console.log(dc.doc);
@@ -44,22 +39,28 @@
       fc.value = processField(fc.value, fc.hkey, fc.highlight, fc.maxlength);
     }
 
+    function handleField(e, docId, score, position) {
+      //dc.postSignal(signalId);
+    }
+
     function processField(field, highlightKey, highlight, maxlength) {
       var result = field;
       var hasHighlight = false;
-      if (highlight && Object.keys(highlight).length > 0){
-        if (highlight[highlightKey]){
+      if (highlight && Object.keys(highlight).length > 0) {
+        if (highlight[highlightKey]) {
           result = highlight[highlightKey];
         }
       }
-      //only shorten if not highlighting, since highlighing in solr can control snippet size there and b/c we have a Trusted object and not a plain old string
-      if (hasHighlight === false && result && result.length > maxlength){
-        //trim it, ideally on whitespace
+      // Only shorten if not highlighting, since highlighing in solr can control
+      // snippet size there and b/c we have a Trusted object and not a plain
+      // old string.
+      if (hasHighlight === false && result && result.length > maxlength) {
+        // Trim it, ideally on whitespace
         var idx = result.lastIndexOf(' ', maxlength);
-        if (idx != -1){
+        if (idx !== -1) {
           result = result.substring(0, idx);
         } else {
-          //we can't find simple whitespace, so just trim arbitrarily
+          // We can't find simple whitespace, so just trim arbitrarily
           result = result.substring(0, maxlength);
         }
         result += '...';
