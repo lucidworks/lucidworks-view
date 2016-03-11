76ba6de65703fb6ed72cd2f9914832a49e30465c
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-03-11 15:51:36 -0800

    WIP document twitter

---
+++ client/assets/components/document_twitter/document_twitter.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fusionSeedApp.components.document_twitter', ['fusionSeedApp.services.signals'])
+    .directive('documentTwitter', documentTwitter);
+
+  /* @ngInject */
+  function documentTwitter() {
+    var directive = {
+      restrict: 'EA',
+      templateUrl: 'assets/components/document_twitter/document_twitter.html',
+      scope: true,
+      controller: Controller,
+      controllerAs: 'vm',
+      bindToController: {
+        doc: '='
+      }
+    };
+
+    return directive;
+
+  }
+
+  function Controller(SignalsService, $log) {
+    'ngInject';
+    var vm = this;
+
+    activate();
+
+    function activate() {
+      vm.postSignal = SignalsService.postSignal;
+      $log.debug('twitter');
+    }
+  }
+})();
