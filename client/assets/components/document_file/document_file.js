dc4ee92a2001b4957ce2662fa4b49a4aec332e84
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-03-11 15:51:16 -0800

    Add document file

---
+++ client/assets/components/document_file/document_file.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fusionSeedApp.components.document_file', ['fusionSeedApp.services.signals'])
+    .directive('documentFile', documentFile);
+
+  /* @ngInject */
+  function documentFile() {
+    var directive = {
+      restrict: 'EA',
+      templateUrl: 'assets/components/document_file/document_file.html',
+      scope: true,
+      controller: Controller,
+      controllerAs: 'vm',
+      bindToController: {
+        doc: '='
+      }
+    };
+
+    return directive;
+
+  }
+
+  function Controller(SignalsService, $log) {
+    'ngInject';
+    var vm = this;
+
+    activate();
+
+    function activate() {
+      vm.postSignal = SignalsService.postSignal;
+      $log.debug('anda.file');
+    }
+  }
+})();
