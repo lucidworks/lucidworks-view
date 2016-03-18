1a62fc06650905997b7b1651e5860d5b9e436904
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-03-18 15:56:16 -0700

    add fallback info to bash script

--- tiara.sh
+++ tiara.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 
+echo -e "        o O o\n" "      o \|/ o\n" "   o o \ | / o o\n" "    \ \ \|/ / /\n" "    (+++\@/+++)\n" "    '---------'\n" "       Tiara"
+
+FILE="$(pwd)/lib/nodejs/bin/npm"
+if ! [ -f "$FILE" ];
+then
+  echo "Use npm instead of tiara.sh when not using packaged version of Tiara." >&2
+  exit
+fi
+
+
 # Dirty ugly hack to make gulp work
 export PATH=$(pwd)/node_modules/.bin:$PATH
-echo -e "        o O o\n" "      o \|/ o\n" "   o o \ | / o o\n" "    \ \ \|/ / /\n" "    (+++\@/+++)\n" "    '---------'\n" "Tiara"
+
 ./lib/nodejs/bin/npm $1 $2 $3
