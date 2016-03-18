40f095257c0b84505e345075ae18482b13acd71f
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-03-18 15:07:57 -0700

    Improved build

--- gulp/package.js
+++ gulp/package.js
@@ -43,7 +43,11 @@ var buildTargets  = {
 var fileLocations = {
   bower: ['bower_components/*/**'],
   node_modules: ['node_modules/*/**'],
-  client: ['client/*/**'],
+  client: [
+    'client/**/**',
+    '!**/.DS_Store',
+    '!.DS_Store'
+  ],
   docs: ['docs/**/*'],
   gulp: ['gulp/**/*', '!gulp/package.js'],
   tests: ['tests/**/*'],
@@ -68,42 +72,37 @@ var os_target = buildTargets.mac;
 // Builds the entire app for deployment
 gulp.task('package', function(cb) {
   // sequence('clean:package', 'download:node', 'move:node', 'move:app', cb);
-  sequence('clean:package', ['download:node', 'move:app'], 'package:bashCommands', cb);
+  sequence('clean:package', ['package:setupTarget','move:app'], 'package:bashCommands', cb);
 });
 
-gulp.task('download:node', function(cb){
+gulp.task('package:setupTarget', function(cb){
   os_target.name = argv.buildname ? argv.buildname: 'default';
   os_target.nodeVersion = argv.nodeVersion ? argv.nodeVersion: os_target.nodeVersion;
   os_target.os = argv.os ? argv.os: os_target.os;
   os_target.platform = argv.platform ? argv.platform: os_target.platform;
   os_target.extension = argv.extension ? argv.extension: os_target.extension;
   console.log(os_target);
-  gulpDownloadTarget(os_target);
   cb();
 });
 
-gulp.task('download:node:mac', function(cb){
+gulp.task('package:setupTarget:mac', function(cb){
   os_target = buildTargets.mac;
-  gulpDownloadTarget(os_target);
   cb();
 });
-gulp.task('download:node:linux', function(cb){
+gulp.task('package:setupTarget:linux', function(cb){
   os_target = buildTargets.linux;
-  gulpDownloadTarget(os_target);
   cb();
 });
-gulp.task('download:node:linux32', function(cb){
+gulp.task('package:setupTarget:linux32', function(cb){
   os_target = buildTargets.linux32;
-  gulpDownloadTarget(os_target);
   cb();
 });
-gulp.task('download:node:sunos', function(cb){
+gulp.task('package:setupTarget:sunos', function(cb){
   os_target = buildTargets.sunos;
-  gulpDownloadTarget(os_target);
   cb();
 });
 
-gulp.task('move:app', ['move:bower', 'move:node_modules', 'move:client', 'move:docs', 'move:gulp', 'move:tests'], function(cb){
+gulp.task('move:app', ['move:bower', /*'move:node_modules',*/ 'move:client', 'move:docs', 'move:gulp', 'move:tests'], function(cb){
   gulp.src(fileLocations.main_components)
   .pipe(gulp.dest('tmp/tiara'));
   cb();
@@ -139,32 +138,18 @@ gulp.task('move:tests', function(cb){
   cb();
 });
 
-gulp.task('node:cleanup', function(cb){
-  sequence('move:node', 'alias:npm');
-  cb();
-});
-
-gulp.task('move:node', ['alias:npm'], function(cb){
-  // gulp.src('tmp/node/'+packageName(os_target)+'/*/**')
-  //   .pipe(gulp.dest('tmp/tiara/lib/nodejs'));
-  cb();
-});
-
 gulp.task('package:bashCommands', $.shell.task([
     'mkdir -p tmp/node',
     'mkdir -p tmp/node/'+packageName(os_target),
     'mkdir -p tmp/tiara/lib/nodejs',
     'curl -o tmp/node/'+packageName(os_target)+'.'+os_target.extension+' '+buildUrl(os_target),
     'tar -xzf tmp/node/'+packageName(os_target)+'.'+os_target.extension+' -C tmp/tiara/lib/nodejs --strip-components=1',
-    // 'mkdir -p tmp/tiara/node_modules',
     'mkdir -p packages',
-    // 'cp -aR tmp/node/'+packageName(os_target)+'/. tmp/tiara/lib/nodejs',
-    // 'cp -aR node_modules/. tmp/tiara/node_modules',
-    'ln -sf ../lib/node_modules/npm/bin/npm-cli.js tmp/tiara/lib/nodejs/bin/npm',
     'chmod +x tmp/tiara/lib/nodejs/bin/npm',
     'chmod +x tmp/tiara/lib/nodejs/bin/node',
     'chmod +x tmp/tiara/lib/nodejs/lib/node_modules/npm/bin/npm',
-    'tar -zcf packages/tiara-'+os_target.os+'-'+os_target.platform+'.tar.gz tmp/tiara'
+    'cd tmp/tiara && ./tiara.sh install',
+    'cd tmp && tar -zcf ../packages/tiara-'+os_target.os+'-'+os_target.platform+'.tar.gz tiara'
   ], {verbose: true})
 );
 
@@ -186,23 +171,8 @@ function string_src(filename, string) {
   return src;
 }
 
-/**
- * Download nodejs based on a target config.
- * @param  {object}   target The target config.
- * @param  {Function} cb     Callback when completed.
- * @return {[type]}          [description]
- */
-function gulpDownloadTarget(target){
-  // var gunzip = ifExpression((target.extension === 'tar.gz'), $.gunzip, false);
-  // var untar = ifExpression((target.extension === 'tar.gz'), $.untar, false);
-  // return $.download(buildUrl(target))
-  //   .pipe(gunzip)
-  //   .pipe(untar)
-  //   .pipe(gulp.dest('tmp/node'));
-}
-
 function buildUrl(target){
-  //https://nodejs.org/dist/v5.8.0/node-v5.8.0-darwin-x64.tar.gz
+  //Format https://nodejs.org/dist/v5.8.0/node-v5.8.0-darwin-x64.tar.gz
   return 'http://nodejs.org/dist/'+target.nodeVersion+'/'+packageName(target)+'.'+target.extension;
 }
 
