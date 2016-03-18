e87e8d00d898da39b885e335f2cbb73964be5f12
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-03-18 15:28:33 -0700

    Add more magic to tiara.sh

--- tiara.sh
+++ tiara.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-echo $1 $2
-./lib/nodejs/bin/npm $1 $2
+# Dirty ugly hack to make gulp work
+export PATH=$(pwd)/node_modules/.bin:$PATH
+echo -e "        o O o\n" "      o \|/ o\n" "   o o \ | / o o\n" "    \ \ \|/ / /\n" "    (+++\@/+++)\n" "    '---------'\n" "Tiara"
+./lib/nodejs/bin/npm $1 $2 $3
