f249146038d3214df7ad9546cabee987856dc09a
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2016-03-24 01:25:41 +0530

    Fixed maxlength for multivalued fields

--- client/assets/components/field/field.js
+++ client/assets/components/field/field.js
@@ -23,7 +23,7 @@
     };
   }
 
-  function Controller() {
+  function Controller($log) {
     'ngInject';
     var fc = this;
 
@@ -38,6 +38,8 @@
     function processField(field, highlightKey, highlight, maxlength) {
       var result = field;
       var hasHighlight = false;
+
+      maxlength = parseInt(maxlength);
       if (highlight && Object.keys(highlight).length > 0) {
         if (highlight[highlightKey]) {
           result = highlight[highlightKey];
@@ -46,6 +48,10 @@
       // Only shorten if not highlighting, since highlighing in solr can control
       // snippet size there and b/c we have a Trusted object and not a plain
       // old string.
+
+      // If field is multivalued, join the items before trimming
+      result = _.isArray(result)?_.join(result, ' '):result;
+
       if (hasHighlight === false && result && result.length > maxlength) {
         // Trim it, ideally on whitespace
         var idx = result.lastIndexOf(' ', maxlength);
