705c67ffb53dc5ded19736e523e73eb6c5026b77
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2016-05-27 21:24:21 +0530

    updated comments

--- client/assets/components/document/documentConfig.js
+++ client/assets/components/document/documentConfig.js
@@ -8,14 +8,14 @@
        *
        * Syntax:
        *
-       * ['field_name=>raw_string_value','template_id'] will checkif
+       * ['field_name=>raw_string_value','template_id/template_id.html'] will checkif
        * a document has a field `field_name` of the value `raw_string_value`.
-       * If yes chosen template for that document will be `template_id.html`.
+       * If yes chosen template for that document will be `template_id/template_id.html`.
        *
        * You can have more advanced predicates, like...
        * [function(doc){
        *   return some_condition(doc);
-       * }, 'template_id.html']
+       * }, 'template_id/template_id.html']
        * the function will be called with the document as argument
        * and if the function returns `true`
        * the corresponding template will be chosen
@@ -24,10 +24,10 @@
        *
        * You can also inject helpers etc. with ngInject to help writing predicate functions
        *
-       * The template IDs will all be fetched from /client/components/document/templates/*.html
+       * The template IDs will all be fetched from /client/components/document/<template_id>/template_id.html
        * The filename would be the document ID, i.e. shown below as set of default templates.
        *
-       * Also if none of the predicates match, the default template i.e. `document_default` will be chosen.
+       * Also if none of the predicates match, the default template i.e. `document_default/document_default.html` will be chosen.
        *
        */
 
