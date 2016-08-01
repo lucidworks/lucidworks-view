f6680b13b6b1762f5c22f35c89d2f288ec52006d
Joshua Ellinger <joshua.ellinger@lucidworks.com>
2016-08-01 12:35:02 -0700

    Add to fusion config too

--- FUSION_CONFIG.sample.js
+++ FUSION_CONFIG.sample.js
@@ -25,6 +25,9 @@ appConfig = { //eslint-disable-line
 
   proxy_allow_self_signed_cert: false, // Only turn on if you have a self signed proxy in front of fusion.
 
+  // The port from which View will be served. defaults to 3000.
+  server_port: 3000,
+
   // Serve View via https.
   // use_https: true,
   // https: {
