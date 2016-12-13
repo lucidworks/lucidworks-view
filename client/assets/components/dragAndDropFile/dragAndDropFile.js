98199c29729fbfaf2eb363050549b52847e4d7e6
AlexKolonitsky <Alex.Kolonitsky@gmail.com>
2016-12-13 12:18:12 +0300

    Added files to repository

--- client/assets/components/dragAndDropFile/dragAndDropFile.js
+++ client/assets/components/dragAndDropFile/dragAndDropFile.js
@@ -1,3 +1,51 @@
-/**
- * Created by Lenovo on 12.12.16.
- */
+(function () {
+  'use strict';
+
+  angular
+    .module('lucidworksView.components.dragAndDropFile', [])
+    .directive('dragAndDropFile', dragAndDropFile);
+
+    function dragAndDropFile () {
+      'ngInject';
+      return {
+        restrict: 'EA',
+        scope: {ngModel: '=', rightFileType: '='},
+        link: function (scope, element, attrs) {
+          var processDragOverOrEnter;
+
+          processDragOverOrEnter = function (event) {
+            if (event !== null) {
+              event.preventDefault();
+            }
+            event.dataTransfer.effectAllowed = 'copy';
+            return false;
+          };
+          element.bind('dragover', processDragOverOrEnter);
+          element.bind('dragenter', processDragOverOrEnter);
+          element.bind('drop', handleDropEvent);
+
+          function insertText(loadedFile) {
+            console.log("insertText");
+            console.log(scope.ngModel);
+            var content = loadedFile.target.result.split(/,|\n/);
+            console.log(content);
+            var modelContent = scope.ngModel || [];
+            var areaContent = modelContent.concat(content);
+            console.log(areaContent);
+            scope.ngModel = areaContent;
+            scope.$apply();
+          }
+
+          function handleDropEvent(event) {
+
+            if (event !== null) {
+              event.preventDefault();
+            }
+            var reader = new FileReader();
+            reader.onload = insertText;
+            reader.readAsText(event.dataTransfer.files[0]);
+          }
+        }
+      };
+    }
+})();
